c3d5cbfb9d69ccb1a04b72929e5b8926ad393c51
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-01 21:33:59 +0200

    ~ Created Visual Studio CMake project.

---
+++ projects/win/VorbisPlugin/VorbisPlugin.cpp
@@ -0,0 +1,12 @@
+﻿// VorbisPlugin.cpp : Defines the entry point for the application.
+//
+
+#include "VorbisPlugin.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
