ad838da663963007e3a91f2777206708482fbe0c
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-05 15:59:51 +0200

    ~ Checking the parameters in VorbisPluginEncoder.c, writing result to file, reading data from samples, not sdtin.

--- src/VorbisPluginEncoder.c
+++ src/VorbisPluginEncoder.c
@@ -5,11 +5,29 @@
 #include <math.h>
 #include <vorbis/vorbisenc.h>
 
-
 #define READ 1024
-signed char readbuffer[READ * 4 + 44]; /* out of the data segment, not the stack */
 
-int EncodePcmData() {
+long EncodePcmDataToFile(float* samples, long samplesLength, short channels, long frequency, float base_quality, const char* filePath) {
+
+    if (samples == NULL) {
+        return 1;
+    }
+    if (samplesLength <= 0) {
+        return 2;
+    }
+    if (channels != 1 && channels != 2) {
+        return 3;
+    }
+    if (frequency < 44100 || frequency > 192000) {
+        return 4;
+    }
+    if (base_quality < 0 || base_quality > 1) {
+        return 5;
+    }
+    if (filePath == NULL) {
+        return 6;
+    }
+
     ogg_stream_state os; /* take physical pages, weld into a logical stream of packets */
     ogg_page         og; /* one Ogg bitstream page.  Vorbis packets are inside */
     ogg_packet       op; /* one raw packet of data for decode */
@@ -20,8 +38,8 @@ int EncodePcmData() {
     vorbis_dsp_state vd; /* central working state for the packet->PCM decoder */
     vorbis_block     vb; /* local working space for packet->PCM decode */
 
-    int eos = 0, ret;
-    int i, founddata;
+    int eos = 0;
+    int i;
 
     vorbis_info_init(&vi);
 
@@ -54,7 +72,7 @@ int EncodePcmData() {
 
         *********************************************************************/
 
-    ret = vorbis_encode_init_vbr(&vi, 2, 44100, 0.1);
+    int ret = vorbis_encode_init_vbr(&vi, channels, frequency, base_quality);
 
     /* do not continue if setup failed; this can happen if we ask for a
        mode that libVorbis does not support (eg, too low a bitrate, etc,
@@ -76,13 +94,15 @@ int EncodePcmData() {
     srand(time(NULL));
     ogg_stream_init(&os, rand());
 
+    /* Open file stream to write in */
+    FILE* file_stream = fopen(filePath, "w");
+
     /* Vorbis streams begin with three headers; the initial header (with
        most of the codec setup parameters) which is mandated by the Ogg
        bitstream spec.  The second header holds any comment fields.  The
        third header holds the bitstream codebook.  We merely need to
        make the headers, then pass them to libvorbis one at a time;
        libvorbis handles the additional Ogg bitstream constraints */
-
     {
         ogg_packet header;
         ogg_packet header_comm;
@@ -97,20 +117,19 @@ int EncodePcmData() {
         /* This ensures the actual
          * audio data will start on a new page, as per spec
          */
-        while (!eos) {
+        while (1) {
             int result = ogg_stream_flush(&os, &og);
             if (result == 0)break;
-            fwrite(og.header, 1, og.header_len, stdout);
-            fwrite(og.body, 1, og.body_len, stdout);
+            fwrite(og.header, 1, og.header_len, file_stream);
+            fwrite(og.body, 1, og.body_len, file_stream);
         }
 
     }
 
-    while (!eos) {
-        long i;
-        long bytes = fread(readbuffer, 1, READ * 4, stdin); /* stereo hardwired here */
 
-        if (bytes == 0) {
+    for (long j = 0; j < samplesLength && !eos; j = j + READ) {
+
+        if (j >= samplesLength - 1) {
             /* end of file.  this can be done implicitly in the mainline,
                but it's easier to see here in non-clever fashion.
                Tell the library we're at end of stream so that it can handle
@@ -125,11 +144,9 @@ int EncodePcmData() {
             float** buffer = vorbis_analysis_buffer(&vd, READ);
 
             /* uninterleave samples */
-            for (i = 0; i < bytes / 4; i++) {
-                buffer[0][i] = ((readbuffer[i * 4 + 1] << 8) |
-                    (0x00ff & (int)readbuffer[i * 4])) / 32768.f;
-                buffer[1][i] = ((readbuffer[i * 4 + 3] << 8) |
-                    (0x00ff & (int)readbuffer[i * 4 + 2])) / 32768.f;
+            for (i = 0; i < READ; i++) {
+                buffer[0][i] = samples[i];
+                //TODO: buffer[1][i] = ;
             }
 
             /* tell the library how much we actually submitted */
@@ -154,8 +171,8 @@ int EncodePcmData() {
                 while (!eos) {
                     int result = ogg_stream_pageout(&os, &og);
                     if (result == 0)break;
-                    fwrite(og.header, 1, og.header_len, stdout);
-                    fwrite(og.body, 1, og.body_len, stdout);
+                    fwrite(og.header, 1, og.header_len, file_stream);
+                    fwrite(og.body, 1, og.body_len, file_stream);
 
                     /* this could be set above, but for illustrative purposes, I do
                        it here (to show that vorbis does know where the stream ends) */
@@ -168,6 +185,8 @@ int EncodePcmData() {
 
     /* clean up and exit.  vorbis_info_clear() must be called last */
 
+    fclose(file_stream);
+
     ogg_stream_clear(&os);
     vorbis_block_clear(&vb);
     vorbis_dsp_clear(&vd);
@@ -178,5 +197,5 @@ int EncodePcmData() {
        libvorbis.  They're never freed or manipulated directly */
 
     fprintf(stderr, "Done.\n");
-    return(0);
+    return 0;
 }
