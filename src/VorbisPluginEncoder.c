ff0f6fd4df7c693e47bbdde6fd2036837baf6ff7
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-05 15:59:51 +0200

    ~ Checking the parameters in VorbisPluginEncoder.c, writing result to file, reading data from samples, not sdtin.

--- src/VorbisPluginEncoder.c
+++ src/VorbisPluginEncoder.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b38cad8563ea449d9a5b563e08b69829f2109f45d0bc6013da6dc3066a9ad53f
-size 6781
+oid sha256:8f15d09b4c99524e84d70a8cdf915ac1aa7a658d31406fa42cb09e29f0eef29e
+size 7089
