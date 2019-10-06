acb98b9ad1a7d2d3114b4b99b977133377e8908d
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-06 18:15:08 +0200

    ~ Renamed the VorbisPluginEncoder.h to VorbisPlugin.h

---
+++ src/VorbisPlugin.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c136e8efcf66d3bed100746ea6e4d3fe4e758de718f054b42ef96aec167907cd
+size 477
--- src/VorbisPluginEncoder.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dbf809348312bc9707f4faad934dcd069cbae2c4ee7a324f7c7136e94611e2a2
-size 452
