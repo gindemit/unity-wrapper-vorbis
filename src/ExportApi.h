ab04daa4e398be3960b46e45b8ced75678a04a2b
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-07 23:00:25 +0200

    + Added FloatArray.h

---
+++ src/ExportApi.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4441c5d8e995e679e9b87aa7d249ba61b027d731123826c881638135f5088e0
+size 349
---
+++ src/FloatArray.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5a0d5a04e64f45d7f4f4630bb3ab08313261bd255653dfaf7f100691af09ec14
+size 872
