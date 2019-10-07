5acd2096fb536d7faf220b27f29aff8b796c72b7
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-07 23:01:56 +0200

    ~ Loading vorbis file now also works.

--- src/VorbisPlugin.h
+++ src/VorbisPlugin.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdaea091faab0c26456e6dd320c65de1d697434faf60b14cf89b6efb5a3ed7c6
-size 696
+oid sha256:ace19194def869a2f4e7152d17561893dce67f84969c803e2cca922820e4cbce
+size 532
--- src/VorbisPluginDecoder.c
+++ src/VorbisPluginDecoder.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c85bdb201073e033ea88f326141d344b42417acd1246e277098982a79bfc1991
-size 12571
+oid sha256:a6606ea8e9366ca8ad720efb5e453f626cf0faa98ef91b15e29d383f475d4241
+size 2147
