445161b14a8073fc83820bcf6b7a7f390df54e3a
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-19 17:36:22 +0200

    ~ Removed test buffer from VorbisPluginEncoder.c

--- src/VorbisPluginEncoder.c
+++ src/VorbisPluginEncoder.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88d4f27e0614df6a927903cd00786a8b81f6c68807cfa416df55f59a977f8ad2
-size 7523
+oid sha256:3718bfec0ed45908bef3c1812fead772d1135746677a27cb2dfc2a4fb6d95b1e
+size 7397
