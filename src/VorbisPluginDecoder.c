9ab6873a8860cfb943bb37e14bd4a1bc6268552f
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-20 17:09:03 +0200

    ~ Supporting two channels ogg files, both saving and loading

--- src/VorbisPluginDecoder.c
+++ src/VorbisPluginDecoder.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec4eb6f24d8849c2101ee2f5af38ba0be8486461e1bfb749e28de0d92e9d40d8
-size 2161
+oid sha256:7c06faf316f0ad9746ebdf64fb9661a62e10a04c4fc0c86d3a4068022ea2181f
+size 2053
--- src/VorbisPluginEncoder.c
+++ src/VorbisPluginEncoder.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3718bfec0ed45908bef3c1812fead772d1135746677a27cb2dfc2a4fb6d95b1e
-size 7397
+oid sha256:cee27945c332b20537096f07dc63ce4f9eb18c7797eb3ba580120f37a72c5ba4
+size 7460
