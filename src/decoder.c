095762836c1f6d18eb5d0d65d013180f5b698140
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-24 21:28:08 +0200

    - Removed unused decoder.c and encoder.c files

--- src/decoder.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9f9b177b15f261e5f4cbaeb513967e9aab9a57accbb0d7f623cde254677c3f47
-size 11596
--- src/encoder.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:43bf8059e144027f2c260df406a18eea8ded082ebd6313d8d956af5bcac6fbcb
-size 9407
