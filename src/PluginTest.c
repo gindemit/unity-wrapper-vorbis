d20be5d57f8a519fc5c765d0ba307d6620e2a93f
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-28 20:09:06 +0100

    ~ Moved test code to separate function

--- src/PluginTest.c
+++ src/PluginTest.c
@@ -12,8 +12,7 @@ static int nearlyEqual(float a, float b, float epsilon)
     return diff < epsilon;
 }
 
-int main() {
-
+static void TestEncodeToFileDecodeFromFile() {
     EncodePcmDataToFile("1_plugin_test_out_text.ogg", testData, testDataLength, 1, 44100, 0.2, 1024);
     float* samples;
     long samples_filled_length;
@@ -28,5 +27,10 @@ int main() {
         int result = nearlyEqual(testData[i], samples[i], 0.05);
         assert(result);
     }
-    printf("Success");
+    printf("Encode Decode success");
+}
+
+int main() {
+    TestEncodeToFileDecodeFromFile();
+
 }
