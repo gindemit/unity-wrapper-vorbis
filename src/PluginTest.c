e9813a9e2dc66be8db3684ba4ae0918411d7d7c5
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-28 20:09:06 +0100

    ~ Moved test code to separate function

--- src/PluginTest.c
+++ src/PluginTest.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62161aa6cd12136a4d7c3e6bcd023e8496b50bb93925b6c98670e67e0fd71d4b
-size 889
+oid sha256:695800d8e6866b60977fa9fcd0562392c43a74a14d85726a3dc22bd63d483433
+size 995
