d7a50314cfdfd03ff3cf7aab9fae491f3da81e29
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-30 21:35:16 +0100

    ~ Added two functions. Now there are three function to read as from stream from vorbis file.

--- src/VorbisPlugin.h
+++ src/VorbisPlugin.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9eb3750aebd53b0ba0b923fb3a68a3ad9058aaf1f60a9fd09106b03dde553e1a
-size 638
+oid sha256:580ce3db120f335ef5598619719dd975b318f2df3bd0c9123c3ba943fb92f38a
+size 1153
--- src/VorbisPluginDecoder.c
+++ src/VorbisPluginDecoder.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ab55c181d30b352bda6e747846e3597a36514141028191064439927c753ee80
-size 3997
+oid sha256:02876dc7e1754c5d124112ebb7e74063e7eea294b74bf6e5df348a71aeaab4fa
+size 4084
