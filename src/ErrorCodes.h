925e8a2f3faa67e71f583473148cd147ff11fdd1
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-30 21:55:40 +0100

    ~ Changed the error code values to negative

--- src/ErrorCodes.h
+++ src/ErrorCodes.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b5c7e6bd87e80717d77d81c336c52f13b4a90ce177165c834bf747e6beaf1e4
-size 597
+oid sha256:ac6447fef1b4efd7ed5ed06a927f31ec82c1c08a460ce4ff7011b3caa972ffd1
+size 605
