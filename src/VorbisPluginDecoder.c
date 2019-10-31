c49fa0bfbf376d56089a5b6165d14d1f5fafa75c
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-31 21:58:22 +0100

    ~ Fill the samples until max_samples read is reached

--- src/VorbisPluginDecoder.c
+++ src/VorbisPluginDecoder.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54fad86c09aced242d3876dccee3c85b9860c7451882c4438a36b22507836fcb
-size 4110
+oid sha256:066a3345be1a1ad8cc321a70d318fd89679ec925ba8470543eb34cb5451898a0
+size 4405
