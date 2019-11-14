e433c56339447206422023786e0d3b9edce1cc30
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-11-14 21:46:39 +0100

    ~ Compilation fix on Android

--- src/VorbisPluginDecoder.c
+++ src/VorbisPluginDecoder.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:066a3345be1a1ad8cc321a70d318fd89679ec925ba8470543eb34cb5451898a0
-size 4405
+oid sha256:9b5bb4a4cfc1dd1d378a7c6c5b207c9cd85409b005e688ab02064552a2695497
+size 4419
