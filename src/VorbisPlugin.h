7c7bcb80832d0325c7dd36b9c48c0bbee59d9650
Konstantin Gindemit <konstantin@soulside.app>
2021-08-01 18:02:54 +0200

    ~ Fixes on iOS platform: the long type is 8 bytes big, but on the C# side the type int is only 4 bytes big.

--- src/VorbisPlugin.h
+++ src/VorbisPlugin.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:687eba47c4dc7432a3295f067a55dab9d6b4f3ad0f2c2f544314c415ed7a92a7
-size 1156
+oid sha256:802c0add7cd455a85b3eb500589ea12717439831de06f3b4f77ec5f1cc48f2d8
+size 1201
--- src/VorbisPluginDecoder.c
+++ src/VorbisPluginDecoder.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b5bb4a4cfc1dd1d378a7c6c5b207c9cd85409b005e688ab02064552a2695497
-size 4419
+oid sha256:a511720c02a9a91a25aa1b54f467a6ec5eb738d9fa193b3ec0aa1fe32d0e35d3
+size 4450
