bb1984b444ebaf1762d809fcd68f26c07abb78b6
Konstantin Gindemit <k.hindemit@rambler.ru>
2021-10-23 19:13:14 +0200

    ~ Better naming. Renames from pascal case to snake case

--- src/PluginTest.c
+++ src/PluginTest.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c595bf207502dfe74405cc131427ef9196aa000584b6da674aadb7c2183dc3b8
-size 1972
+oid sha256:4564b635db192b6e81a35ec5e6d2c45472c6183e2496894bde99877853951ba6
+size 1989
--- src/TestSamplesArray.h
+++ src/TestSamplesArray.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:533ef95cd45c644943e5956cbe3d0d3ce7f670156d9e54dfbc397075736e5c85
-size 770748
+oid sha256:c9200bfcdcaec29dd219cd9e3a0dfc27553a98eff0addd686539c56dc4efb2ef
+size 770751
