7dbe119b0bbe267e98c14e6ec73068c49084f8ed
Konstantin Gindemit <k.hindemit@rambler.ru>
2021-10-24 12:35:51 +0200

    ~ Don't print vendor and comments messages when ogg is loaded.

--- src/PluginTest.c
+++ src/PluginTest.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cbf3396da4bc6df7db6e89e87f0756df1451b1f61de07afb24834bef4c34430
-size 2815
+oid sha256:e20fabcca98b827fedc50b4f7c412710c959fbb8c3249fad63aa3813f4209b08
+size 2863
--- src/VorbisPluginDecoder.c
+++ src/VorbisPluginDecoder.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0684a4d89f4b27c383f3a3001e5a88cff1bfc228cc17c10f1497d2277feec6f
-size 4539
+oid sha256:58c9cba7402534f9a2018d7a23f9e4fcb5603e2a4d88f6e8a607ee601c8581df
+size 4547
