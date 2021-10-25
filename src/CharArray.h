e62b11dc3adb82c3842dcb8372130ee952ede2ad
Konstantin Gindemit <k.hindemit@rambler.ru>
2021-10-25 23:45:23 +0200

    ~ Renamed and reimplemented the char array to unsigned char array.

--- src/CharArray.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cbd1a4c197e7f84ff4ea4ed84343c55a1ed55c0305e6004c35d1fdd21fcb498c
-size 845
---
+++ src/UnsignedCharArray.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d8e9a9f45a2addca022289f460e8dd4c627d6f29cee398fe7148cf1be8a03314
+size 976
