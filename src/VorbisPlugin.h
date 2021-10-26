aed4f8b73ad3607b6640d59fe7c891e50dc35f7e
Konstantin Gindemit <k.hindemit@rambler.ru>
2021-10-26 23:02:13 +0200

    ~ Better formating in VorbisPlugin.h

--- src/VorbisPlugin.h
+++ src/VorbisPlugin.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d8b5d1aefec384a1bdbb066b0845fe0cc1bca4a8c7ceef1c695443b5bcf18ab
+oid sha256:e210d62057389a7541bed044335bd787a5162b1b84c3576f1e76474246e57048
 size 1816
