1390ac13471929d04a5150b52013610522389f99
Konstantin Gindemit <k.hindemit@rambler.ru>
2021-11-01 22:05:03 +0100

    ~ Error code enum instead of #defines

--- src/ErrorCodes.h
+++ src/ErrorCodes.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f77268f9a881437424c9f7a65c39d11f565cdddef0e5c1ea90c6229d5ee83e6a
-size 691
+oid sha256:6cb9fbb8bacdc99f6ff07bb76c11e241a15222063a4885cbadb72c69def1a420
+size 705
