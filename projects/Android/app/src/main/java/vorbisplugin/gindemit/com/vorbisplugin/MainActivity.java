846119b7107c95e60e1ed88f51416a3273422edd
Konstantin Gindemit <konstantin.gindemit@gmail.com>
2025-11-22 11:41:16 +0100

    ~ Another attempt to fix the build issues

--- projects/Android/app/src/main/java/vorbisplugin/gindemit/com/vorbisplugin/MainActivity.java
+++ projects/Android/app/src/main/java/vorbisplugin/gindemit/com/vorbisplugin/MainActivity.java
@@ -1,6 +1,6 @@
 package vorbisplugin.gindemit.com.vorbisplugin;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
